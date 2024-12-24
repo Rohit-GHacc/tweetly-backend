e224b07be33b165827e2395f15841638c53061aa
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-12-24 14:59:01 +0530

    trying to deploy

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(urlencoded({
     extended: true
 }))
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'https://tweetlys.vercel.app',
+    methods: ['POST','GET','PUT','DELETE'],
     credentials: true
 }))
 app.use(cookieParser());
