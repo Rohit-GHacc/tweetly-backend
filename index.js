f568834c360f036f22baf71e89571fca9e07a0ed
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-12-24 16:20:16 +0530

    ...

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(urlencoded({
     extended: true
 }))
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'http://tweetlys.vercel.app',
     methods: ['POST','GET','PUT','DELETE'],
     credentials: true
 }))
